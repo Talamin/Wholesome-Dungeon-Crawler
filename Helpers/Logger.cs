054e3e4a72219f8bc302932a54166696298aced5
Talamin <52570957+Talamin@users.noreply.github.com>
2022-04-14 09:31:27 +0200

    Added Logger.cs

---
+++ Helpers/Logger.cs
@@ -0,0 +1,24 @@
+﻿using robotManager.Helpful;
+using System.Drawing;
+
+
+namespace WholesomeDungeonCrawler.Helpers
+{
+    class Logger
+    {
+        public static void LogError(string message)
+        {
+            Logging.Write($"[{Main.ProductName}]: {message}", Logging.LogType.Error, Color.DarkRed);
+        }
+
+        public static void Log(string message)
+        {
+            Logging.Write($"[{Main.ProductName}]: {message}", Logging.LogType.Normal, Color.DarkSlateBlue);
+        }
+
+        public static void LogDebug(string message)
+        {
+            Logging.Write($"[{Main.ProductName}]: {message}", Logging.LogType.Debug, Color.DarkGoldenrod);
+        }
+    }
+}
