ff87d546207633cb94ada4e02a3e799e35dd2502
Talamin <52570957+Talamin@users.noreply.github.com>
2022-04-14 07:51:39 +0200

    First Commit

---
+++ Main.cs
@@ -0,0 +1,61 @@
+﻿using robotManager.Products;
+using System;
+using System.Windows.Controls;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using robotManager.FiniteStateMachine;
+using static robotManager.Helpful.Logging;
+using robotManager.Helpful;
+
+public class Main : IProduct
+{
+    private static readonly Engine FSM = new Engine();
+    public static readonly string ProductName = "Wholesome Dungeon Crawler";
+    public System.Windows.Controls.UserControl Settings => throw new NotImplementedException();
+
+    public bool IsStarted => throw new NotImplementedException();
+
+    public void Initialize()
+    {
+        try
+        { 
+
+        }
+        catch(Exception e)
+        {
+            WriteDebug("Main -> Initialize(): " + e);
+        }
+    }
+
+    public void Start()
+    {
+        try
+        {
+
+        }
+        catch (Exception e)
+        {
+            WriteDebug("Main -> Start(): " + e);
+        }
+    }
+
+    public void Stop()
+    {
+        try
+        {
+
+        }
+        catch (Exception e)
+        {
+            WriteDebug("Main -> Stop(): " + e);
+        }
+    }
+
+    public void Dispose()
+    {
+        throw new NotImplementedException();
+    }
+}
+
