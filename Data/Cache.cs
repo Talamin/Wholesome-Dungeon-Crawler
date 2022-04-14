760cc43a1919fe99e5d3620a53c397605c08eaad
Talamin <52570957+Talamin@users.noreply.github.com>
2022-04-14 16:04:17 +0200

    reworked Cache

--- Data/Cache.cs
+++ Data/Cache.cs
@@ -4,42 +4,39 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using WholesomeToolbox;
+using wManager.Events;
 using wManager.Wow.ObjectManager;
 
 namespace WholesomeDungeonCrawler.Data
 {
-    public class Cache
+    internal class Cache
     {
-        public static bool Initialized { get; private set; }
 
         public static bool cIsInInstance { get; private set; }
 
         private static object _cacheLock = new object();
 
-        public static void Initialize()
-        {
-            lock(_cacheLock)
-            {
-                cacheGeneral();
-                Initialized = true;
-            }
-        }
+        public static void Initialize() => ObjectManagerEvents.OnObjectManagerPulsed += OnObjectManagerPulse;
 
-        public static void Dispose()
+        public static void Dispose() => ObjectManagerEvents.OnObjectManagerPulsed -= OnObjectManagerPulse;
+
+
+        private static void OnObjectManagerPulse()
         {
             lock (_cacheLock)
             {
-                Initialized = false;
+                cacheGeneral();
             }
+
         }
 
         private static void cacheGeneral()
         {
-            lock(_cacheLock)
+            lock (_cacheLock)
             {
                 cIsInInstance = WTLocation.IsInInstance();
             }
-            
+
         }
 
     }
