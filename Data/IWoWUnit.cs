cfd211a4998080ccd1cd92216f227e4407884fc1
Talamin <52570957+Talamin@users.noreply.github.com>
2022-04-15 22:00:11 +0200

    Added IWoWUnit

---
+++ Data/IWoWUnit.cs
@@ -0,0 +1,20 @@
+﻿using robotManager.Helpful;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WholesomeDungeonCrawler.Data
+{
+    interface IWoWUnit
+    {
+        ulong Guid { get; }
+        ulong TargetGuid { get; }
+        string Name { get; }
+        bool Valid { get; }
+        bool Alive { get; }
+        Vector3 PositionWithoutType { get; }
+        int Reaction { get; } 
+    }
+}
