80bbd96248e92c5abbd24862e725bb3326f3cd86
Talamin <52570957+Talamin@users.noreply.github.com>
2022-04-22 08:27:02 +0200

    Added  Movehelper Class Added MoveAlongPath To Step Class Added MoveAlontPath to Movehelper  Class

---
+++ Helpers/MoveHelper.cs
@@ -0,0 +1,107 @@
+﻿using robotManager.Helpful;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using WholesomeToolbox;
+using wManager.Wow.Bot.Tasks;
+using wManager.Wow.Helpers;
+
+namespace WholesomeDungeonCrawler.Helpers
+{
+    public class MoveHelper
+    {
+        private static readonly Vector3 Empty = Vector3.Empty;
+        private static readonly object Lock = new object();
+        private static bool Running = false;
+
+        private static void Wait()
+        {
+            if (CurrentTarget != Empty)
+            {
+                Running = false;
+            }
+        }
+
+        public static void StopAllMove(bool stopWalking = false)
+        {
+            lock (Lock)
+            {
+                Wait();
+            }
+            if (stopWalking)
+            {
+                MovementManager.StopMove();
+                MovementManager.StopMoveTo();
+            }
+        }
+
+        public static Vector3 CurrentTarget { get; private set; } = Empty;
+
+        public static bool IsMovementThreadRunning => CurrentTarget != Empty/* || MovementManager.CurrentPath.Count > 0*/;
+
+        public static void StartGoToThread(Vector3 target, string log = null)
+        {
+            lock (Lock)
+            {
+                if (CurrentTarget.Equals(target))
+                {
+                    return;
+                }
+
+                Wait();
+
+                CurrentTarget = target;
+                Running = true;
+                ThreadPool.QueueUserWorkItem(_ =>
+                {
+                    if (log != null)
+                    {
+                        Logger.Log(log);
+                    }
+                    GoToTask.ToPosition(target, 0.5f, conditionExit: _ => Running);
+                    //Logger.LogDebug($"GoToTask finished towards {target}");
+                    lock (Lock)
+                    {
+                        CurrentTarget = Empty;
+                        Monitor.Pulse(Lock);
+                    }
+                });
+            }
+        }
+
+        public static void StartMoveAlongThread(List<Vector3> path, string log = null)
+        {
+            lock (Lock)
+            {
+                if (CurrentTarget.Equals(MovementManager.CurrentMoveTo))
+                {
+                    return;
+                }
+
+                var pathIndex  = WTPathFinder.GetIndexOfClosestPoint(path);
+
+                Wait();
+
+                CurrentTarget = path[pathIndex];
+                Running = true;
+                ThreadPool.QueueUserWorkItem(_ =>
+                {
+                    if (log != null)
+                    {
+                        Logger.Log(log);
+                    }
+                    GoToTask.ToPosition(MovementManager.CurrentMoveTo, 0.5f, conditionExit: _ => Running);
+                    //Logger.LogDebug($"GoToTask finished towards {target}");
+                    lock (Lock)
+                    {
+                        CurrentTarget = Empty;
+                        Monitor.Pulse(Lock);
+                    }
+                });
+            }
+        }
+    }
+}
---
+++ Profiles/Base/MoveAlongPath.cs
@@ -0,0 +1,60 @@
+﻿using robotManager.Helpful;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WholesomeDungeonCrawler.Dungeonlogic;
+using WholesomeDungeonCrawler.Helpers;
+using wManager.Wow.Helpers;
+using wManager.Wow.ObjectManager;
+
+namespace WholesomeDungeonCrawler.Profiles.Base
+{
+    internal class MoveAlongPath : Step
+    {
+        private readonly List<Vector3> _path;
+        private readonly float _randomization;
+        private readonly Vector3 _target;
+
+        public MoveAlongPath(List<Vector3> path, string stepName = "MoveAlongPath", float randomization = 0) :
+            base(stepName)
+        {
+            _path = path;
+            _randomization = randomization;
+            _target = path.LastOrDefault();
+        }
+
+        private static List<Vector3> PathFromClosestPoint(List<Vector3> path)
+        {
+            var sortingList = new Tuple<ushort, Vector3>[path.Count];
+
+            for (ushort i = 0; i < path.Count; i++) sortingList[i] = new Tuple<ushort, Vector3>(i, path[i]);
+
+            Vector3 myPosition = ObjectManager.Me.PositionWithoutType;
+            ushort startIndex = sortingList.OrderBy(tuple => tuple.Item2.DistanceTo(myPosition))
+                .FirstOrDefault(tuple => !TraceLine.TraceLineGo(tuple.Item2))?.Item1 ?? 0;
+
+            if (startIndex != 0) Logger.LogDebug($"Skipped the first {startIndex} steps of the path.");
+
+            return startIndex != 0 ? path.GetRange(startIndex, path.Count - startIndex) : path;
+        }
+
+        public override bool Pulse()
+        {
+            if (ObjectManager.Me.PositionWithoutType.DistanceTo(_target) < 5f)
+            {
+                IsCompleted = true;
+                return true;
+            }
+
+            if (!MoveHelper.IsMovementThreadRunning || MoveHelper.CurrentTarget.DistanceTo(_target) > 2)
+            {
+                MoveHelper.StartMoveAlongThread(PathFromClosestPoint(_path));
+            }
+
+            return IsCompleted;
+        }
+
+    }
+}
