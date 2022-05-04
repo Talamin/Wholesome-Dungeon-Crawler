ddd490d48c7f985cf140fe43079928c887e5907a
Talamin <52570957+Talamin@users.noreply.github.com>
2022-05-04 15:03:37 +0200

    Added more  Models (like  Heidi Klum)

---
+++ Data/Model/DungeonModel.cs
@@ -0,0 +1,18 @@
+﻿using robotManager.Helpful;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WholesomeDungeonCrawler.Data.Model
+{
+    class DungeonModel
+    {
+        public string Name { get; }
+        public int MapId { get; }
+        public int DungeonId { get; }
+        public Vector3 Start { get; }
+        public Vector3 EntranceLoc  { get; }
+}
+}
---
+++ Data/Model/StepModel.cs
@@ -0,0 +1,23 @@
+﻿using Newtonsoft.Json;
+using robotManager.Helpful;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WholesomeDungeonCrawler.Data.Model
+{
+    class StepModel
+    {
+        [JsonProperty("$type")]
+        public string Type { get;}
+        public bool IsCompleted { get; }
+        public List<Vector3> Path { get; }
+        public double Randomization { get; }
+        public object Target { get; }
+        public bool OverrideNeedToRun { get;}
+        public string Name { get;}
+        public object Order { get; }
+    }
+}
