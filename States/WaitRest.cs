2a8519e3ff4d0079fa20760b66dd6d0e9c91245d
Talamin <52570957+Talamin@users.noreply.github.com>
2022-06-01 21:21:42 +0200

    Added Ghost Debuff for Tank check

--- States/WaitRest.cs
+++ States/WaitRest.cs
@@ -37,7 +37,7 @@ namespace WholesomeDungeonCrawler.States
                     return false;
                 }
 
-                return _entityCache.ListGroupMember.Any(y => y.HasDrinkBuff || y.HasFoodBuff || y.PositionWithoutType.DistanceTo(_entityCache.Me.PositionWithoutType) >= 40);
+                return _entityCache.ListGroupMember.Any(y => y.HasDrinkBuff || y.HasFoodBuff || y.Auras.ContainsKey(8326) /*Ghost*/ || y.PositionWithoutType.DistanceTo(_entityCache.Me.PositionWithoutType) >= 40);
             }
         }
         public override void Run()
