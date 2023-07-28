f8a982d39f6ceea7a4d20d3d113fad5a502362a1
Wholesome-wRobot <jeanlouisledaron@gmail.com>
2023-07-28 21:03:41 +0200

    Removed dungeon starts

--- Models/DungeonModel.cs
+++ Models/DungeonModel.cs
@@ -8,7 +8,6 @@ namespace WholesomeDungeonCrawler.Models
         public int MapId { get; set; }
         public int DungeonId { get; set; }
         public int ContinentId { get; set; }
-        public Vector3 Start { get; set; }
         public Vector3 EntranceLoc { get; set; }
         public bool IsHeroic { get; set; }
     }
